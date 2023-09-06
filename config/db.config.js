03bafbbe547c6b08156f4b7e179b7caff9133c11
031-niti <earthza6263@gmail.com>
2023-09-06 15:49:39 +0700

    edit

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "aws.connect.psdb.cloud",
-    USER : "g759c1os1s8jlbjeof2q",
-    PASSWORD : "pscale_pw_CAAXV9KgXqwYrQX82DNl7cAlGmav1ufYG4K5x3wAQkH",
+    USER : "xuldxpisyfc2kidvftie",
+    PASSWORD : "pscale_pw_OdngRkDtfFxwudpFHeiz177b5kQd4mBqi0NgZSSwdE0",
     DB : "se_database"
 }
