f6a0568a0d20d22244060e8e30d736dd31d201d8
031-niti <earthza6263@gmail.com>
2023-09-11 09:40:46 +0700

    edit

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST: "aws.connect.psdb.cloud",
-    USER : "amuxpgfjvatqcliu6uny",
-    PASSWORD : "pscale_pw_rkKe8Vw8WrrbojRg2D1JXGYHYXXZjet19Advpj0ZOsk",
-    DB : "se_database"
-}
+    HOST: process.env.HOST,
+    USER : process.env.USER,
+    PASSWORD : process.env.PASSWORD,
+    DB : process.env.DB
+};
